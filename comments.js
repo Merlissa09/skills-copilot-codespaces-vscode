23862a04111610f518679265d6594729b01fdeef
Merlissa09 <41809281+Merlissa09@users.noreply.github.com>
2023-06-01 17:37:40 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,26 +1,26 @@
 // create web server
-// start web server
-// handle requests
-// handle responses
-// send responses
-// close web server
-
-// 1. create web server
-var express = require('express');
-var app = express();
-
-// 2. start web server
-var port = process.env.PORT || 3000;
-app.listen(port, function() {
-  console.log('Server started on port ' + port);
+// 1. load http module
+var http = require('http');
+// 2. create web server object
+var server = http.createServer();
+// 3. start web server with port #
+server.listen(3000, function() {
+    console.log('Server is running on port 3000');
 });
-
-// 3. handle requests
-// GET /comments
-app.get('/comments', function(request, response) {
-  // 4. handle responses
-  var comments = [
-    {
-      id: 1,
-    }
-]});
+// 4. handling request event
+server.on('connection', function(socket) {
+    console.log('Client connected');
+});
+server.on('request', function(req, res) {
+    console.log('Client request');
+    res.writeHead(200, {'Content-Type': 'text/html; charset=utf-8'});
+    res.write('<h1>안녕하세요</h1>');
+    res.end();
+});
+server.on('close', function() {
+    console.log('Server closed');
+});
+// 5. handling error event
+server.on('error', function(err) {
+    console.log('Error: %s', err.message);
+});
